636145dc5d24fe8d545da4bdc3fb60bfc4b42dbf
gondar <mbazydlo@opentable.com>
2015-01-21 10:06:06 +0000

    Updated Game tests to cover actual class responsibility.

--- BowlingGameTests/GameTests.cs
+++ BowlingGameTests/GameTests.cs
@@ -19,6 +19,13 @@ namespace BowlingGameTests
             return frame;
         }
 
+        internal static IFrame GetFrame(int score, IFrame nextFrame)
+        {
+            var frame = Substitute.For<IFrame>();
+            frame.GetScore(nextFrame).Returns(score);
+            return frame;
+        }
+
         [TestFixture]
         public class When_playing_game : With_Game
         {
@@ -34,9 +41,75 @@ namespace BowlingGameTests
             }
 
             [Test]
-            public void should_return_zero()
+            public void should_return_valid_outcome()
             {
                 Outcome.ShouldBe(20);
+            }
+        }
+
+        [TestFixture]
+        public class When_playing_game_with_frames_depending_on_subsequent_frames : With_Game
+        {
+            private IFrame _frameAfterSpare;
+            private IFrame _frameWithSpare;
+
+            [TestFixtureSetUp]
+            public void SetUp()
+            {
+                _frameAfterSpare = GetFrame(5);
+                _frameWithSpare = GetFrame(15, _frameAfterSpare);
+
+                CurrentGameState.Frames.Add(_frameWithSpare);
+                CurrentGameState.Frames.Add(_frameAfterSpare);
+                for (var i = 0; i < 8; i++)
+                {
+                    CurrentGameState.Frames.Add(GetFrame(1));
+                }
+
+                Outcome = Subject.Play(CurrentGameState);
+            }
+
+            [Test]
+            public void should_return_valid_outcome()
+            {
+                Outcome.ShouldBe(28);
+            }
+
+            [Test]
+            public void should_pass_next_frame()
+            {
+                _frameWithSpare.Received().GetScore(_frameAfterSpare);
+            }
+        }
+
+        [TestFixture]
+        public class When_playing_game_with_frame_depending_on_subsequent_frame_but_it_is_last_frame : With_Game
+        {
+            private IFrame _frameWithSpare;
+
+            [TestFixtureSetUp]
+            public void SetUp()
+            {
+                for (var i = 0; i < 9; i++)
+                {
+                    CurrentGameState.Frames.Add(GetFrame(1));
+                }
+                _frameWithSpare = GetFrame(15, null);
+                CurrentGameState.Frames.Add(_frameWithSpare);
+
+                Outcome = Subject.Play(CurrentGameState);
+            }
+
+            [Test]
+            public void should_return_valid_outcome()
+            {
+                Outcome.ShouldBe(24);
+            }
+
+            [Test]
+            public void should_pass_null_as_next_frame()
+            {
+                _frameWithSpare.Received().GetScore(null);
             }
         }
 
