6df823d12ff690e4129dd68a8e0f70b5852f3dcd
gondar <mbazydlo@opentable.com>
2015-02-20 13:46:53 +0000

    Added frame parser basic test

---
+++ BowlingGame/UI/FrameParser.cs
@@ -0,0 +1,25 @@
+﻿using BowlingGame.Domain;
+
+namespace BowlingGame.UI
+{
+    public class FrameParser
+    {
+        public IFrame Parse(string frameText)
+        {
+            var rolls = frameText.Trim().Split(' ');
+            var roll1 = parseFirstRoll(rolls[0]);
+            var roll2 = parseFirstRoll(rolls[1]);
+
+            return new Frame()
+            {
+                RollOne = roll1,
+                RollTwo = roll2
+            };
+        }
+
+        private int parseFirstRoll(string roll)
+        {
+            return int.Parse(roll);
+        }
+    }
+}
---
+++ BowlingGameTests/UI/FrameParserTests.cs
@@ -0,0 +1,38 @@
+﻿using BowlingGame.Domain;
+using BowlingGame.UI;
+using NUnit.Framework;
+using Shouldly;
+
+namespace BowlingGameTests.UI
+{
+    class FrameParserTests
+    {
+        [TestFixture]
+        public class When_parsing_single_frame
+        {
+            private IFrame _outcome;
+
+            [TestFixtureSetUp]
+            public void SetUp()
+            {
+                const string frameText = " 1 2 ";
+                var subject = new FrameParser();
+
+                _outcome = subject.Parse(frameText);
+            }
+
+            [Test]
+            public void should_return_valid_first_roll_value()
+            {
+                _outcome.RollOne.ShouldBe(1);
+            }
+
+            [Test]
+            public void should_return_valid_second_roll_value()
+            {
+                _outcome.RollTwo.ShouldBe(2);
+            }
+        }
+
+    }
+}
